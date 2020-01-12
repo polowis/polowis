76e4312c8ab41a1ffde3ad3a97b7abf53ab020d4
polowis <tranphuochung0912@gmail.com>
2020-01-12 16:57:38 +1100

    Initial commit

---
+++ src/introdution.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+class Introduction extends React.Component{
+    render(){
+        const ImgStyle = {
+            position: "relative", 
+            top: "75px", 
+            zIndex: "1"
+            
+        }
+
+        const boxStyle ={
+            maxWidth: "500px", 
+            paddingTop: "75px", 
+            marginTop: "0px"
+        }
+        const mainStyle = {
+            height: "100%",
+            marginTop: "-75px"
+        }
+        return(
+            <div style={mainStyle} class="ui middle aligned grid ">
+
+    <div class="column">
+
+    <div style={ImgStyle} class="ui fluid centered small circular image">
+      <img src="https://avatars0.githubusercontent.com/u/39208974?s=460&v=4"/>
+    </div>
+
+
+    <div style={boxStyle} class="ui fluid centered card">
+
+      <div class="center aligned content">
+        <span class="header">Hung Tran</span>
+        <div class="meta">
+          <span class="date">Student</span>
+        </div>
+        <div class="left aligned description">
+          I am studying at Gungahlin College, I expect to graduate in December, 2020
+        </div>
+      </div>
+      <div class="center aligned content">
+        <div class="description">
+          Interests:
+          
+            Data Science,
+            Web development
+          
+
+        </div>
+      </div>
+      <div class="center aligned extra content">
+
+
+   <a href="http://github.com/polowis"> <i class="big github icon icon-color"></i> </a>
+
+   <a href="http://linkedin.com/in/changeme"> <i class="big linkedin icon icon-color"></i> </a>
+
+   <a href="http://twitter.com/changeme"> <i class="big twitter icon icon-color"></i> </a>
+
+   <a href="http://instagram.com/changeme"> <i class="big instagram icon icon-color"></i> </a>
+
+
+
+  <a href="mailto:molly@hawaii.edu"> <i class="big mail square icon icon-color"></i> </a>
+
+      </div>
+      <div class="center aligned content">
+        <a href="/projects">
+          <button class="ui basic button">Projects (3)</button>
+        </a>
+        <a href="/template/essays/">
+          <button class="ui basic button">Essays (3)</button>
+        </a>
+        <a href="/template/bio/">
+          <button class="ui basic button">Resume</button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+</div>
+        );
+    }
+}
+
+export default Introduction;
